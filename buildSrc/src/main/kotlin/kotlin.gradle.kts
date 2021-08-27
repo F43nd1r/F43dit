20e087d42bcb416b42ca66b7256d66f74166c846
f43nd1r <account+github@faendir.com>
2021-08-27 18:17:44 +0200

    fix module jvm target

--- buildSrc/src/main/kotlin/kotlin.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin.gradle.kts
@@ -7,6 +7,11 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
