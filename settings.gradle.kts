5310cdfe6c483bf94980876c30dd17b2f846a0ec
f43nd1r <account+github@faendir.com>
2023-09-03 13:38:03 +0200

    setup renovate

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 enableFeaturePreview("VERSION_CATALOGS")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-rootProject.name = "zachtronics-leaderboard-bot"
+rootProject.name = "F43dit"
 rootDir.listFiles()?.forEach {
     if(it.isDirectory && it.name != "buildSrc" && it.list()?.contains("build.gradle.kts") == true) {
         include(it.name)
